2540a866454fdd40670f2e99bdaa83066f7de607
kayubakar0 <57085987+kayubakar0@users.noreply.github.com>
2021-03-16 10:12:28 +0700

    update tugas

---
+++ Pertemuan2/Teori/bukudemo1.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author adrian
+ */
+public class bukudemo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        System.out.println("Buku 1");
+        buku buku1 = new buku("Pemrograman Berbasis Objek dengan Java", "Indrajani", "Elexmedia Komputindo", 2007);
+        buku1.cetakbuku();
+        System.out.println("Buku 1");
+        buku buku2 = new buku("Dasar Pemrograman Java", "Abdul Kadir", "Andi Offset", 2004);
+        buku2.cetakbuku();
+    }
+}
+
+class buku{
+    private String judul;
+    private String pengarang;
+    private String penerbit;
+    private int tahun;
+    
+    public buku(String judul, String pengarang, String penerbit,int tahun){
+        this.judul = judul;
+        this.pengarang = pengarang;
+        this.penerbit = penerbit;
+        this.tahun = tahun;
+    }
+    public void cetakbuku(){
+        System.out.println("Judul      :  " + this.judul);
+        System.out.println("Pengarang  :  " + this.pengarang);
+        System.out.println("Penerbit   :  " + this.penerbit);
+        System.out.println("Tahun      :  " + this.tahun);
+    }
+}
